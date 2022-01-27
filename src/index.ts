112dcd2907e759f3bd988d0012ec1e888e625a2b
Henrik Herbst Sørensen <hhs@trifork.com>
2022-01-27 13:54:43 +0100

    Use service registry

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,20 @@
-import fetch from 'node-fetch';
-import {Stats} from "./model/stats";
-import {getStats} from "./service/stat-service";
-import {DateTime, Duration, Interval} from "luxon";
-import {Account} from "./model/account";
+import {accountJob, statsService} from "./service/stat-service";
+import {destructServices, initServices, registerService} from "./config/service-registry";
+import {influxService} from "./service/influx-service";
+import {createScheduleService} from "./service/schedule-service";
 
-const schedule = require('node-schedule');
+registerService(statsService)
+registerService(influxService);
+registerService(createScheduleService(accountJob));
 
-const accounts: Account[] = [
-    {
-        handle: 'fantastiskefroe.dk',
-        nextTime: null
-    },
-    {
-        handle: 'spirekassen',
-        nextTime: null
-    },
-    {
-        handle: 'froesnapperen',
-        nextTime: null
-    }
-];
+initServices();
 
-const fullUpdateCycle = Duration.fromObject({seconds: 30});
-const timeBetweenAccounts = Duration.fromObject({seconds: fullUpdateCycle.as("seconds") / (accounts.length)});
+process.on('SIGINT', handleTermination);
+process.on('SIGTERM', handleTermination);
 
-initAccounts();
-
-const job = schedule.scheduleJob('* * * * * *', () => {
-    const now: DateTime = DateTime.now();
-
-    for (let i = 0; i < accounts.length; i++){
-        const account = accounts[i];
-        const previousAccount = accounts[i === 0 ? accounts.length - 1 : i - 1]
-
-        if (account.nextTime <= now) {
-            getStats(account.handle)
-                .then(stats => {
-                    console.log(now.toISO(), stats);
-                })
-
-            account.nextTime = previousAccount.nextTime.plus(timeBetweenAccounts).plus(randomJitter(5));
-        }
-    }
-});
-
-function initAccounts(): void {
-    const now: DateTime = DateTime.now();
-    let nextTime = now.plus(timeBetweenAccounts).plus(randomJitter(5));
-
-    for (let i = 0; i < accounts.length; i++) {
-        accounts[i].nextTime = nextTime;
-        nextTime = nextTime.plus(timeBetweenAccounts).plus(randomJitter(5));
-    }
+function handleTermination(args) {
+    console.info(`Received ${args} shutting down`);
+    destructServices()
+        .then(() => process.exit(0));
 }
 
-function randomJitter(max: number = 0): Duration {
-    return Duration.fromObject({
-        seconds: Math.round(Math.random() * 2 * max - max)
-    });
-}
-
-process.on('SIGINT', () => {
-    schedule.gracefulShutdown()
-        .then(() => process.exit(0))
-});
